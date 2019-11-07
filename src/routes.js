c2db1a7389f4d83bafdbfef4ff9c67399705b923
Leonel <leonelpelusi@gmail.com>
2019-11-07 14:51:06 -0300

    User Controller/Database config

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/model/User';
+
+import UserController from './app/controller/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Leo',
-    email: 'leo@gmail.com',
-    password_hash: '123456',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
