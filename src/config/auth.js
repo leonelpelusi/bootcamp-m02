09f2be145945846fe9e214ba405a3c0e4dab7034
Leonel <leonelpelusi@gmail.com>
2019-11-11 15:57:14 -0300

    Session Controller

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '2d210fa0c9921259ed38f78f4dd1be5f',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import UserController from './app/controller/UserController';
+import SessionControler from './app/controller/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionControler.store);
 
 export default routes;
